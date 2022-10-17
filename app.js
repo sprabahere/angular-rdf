461bfd6087d4e64cdf12af12b223d130417f53b5
sjeganit <115979381+sjeganit@users.noreply.github.com>
2022-10-17 15:11:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 function test(){
 alert('hello');
+  angular.module('F1FeederApp', [
+  'F1FeederApp.controllers',
+  'F1FeederApp.services'
+]);
 }
 angular.module('F1FeederApp', [
   'F1FeederApp.controllers'
